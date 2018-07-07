82d45682586f14262a0a7a19e634afa225c4de89
iamdevakumar <devakumar.indian@gmail.com>
2018-07-07 10:56:50 +0530

    Call intent fixed 06/07/18

--- app/src/main/java/com/app/starautoassist/Activity/TrackerActivity.java
+++ app/src/main/java/com/app/starautoassist/Activity/TrackerActivity.java
@@ -4,23 +4,40 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.app.starautoassist.Others.Starautoassist_Application;
+import com.app.starautoassist.R;
 import com.app.starautoassist.Services.Tracker_Service;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class TrackerActivity extends Activity {
-
+public class TrackerActivity extends AppCompatActivity implements LocationListener, OnMapReadyCallback {
+    SupportMapFragment mapFragment;
+    private LocationManager locationManager;
+    String provider;
     private static final int PERMISSIONS_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracking);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(TrackerActivity.this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
 
         // Check GPS is enabled
         LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
@@ -77,5 +94,14 @@ public class TrackerActivity extends Activity {
         } else {
             finish();
         }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
